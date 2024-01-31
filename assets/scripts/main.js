8e0ef928f476e3e02a4e60ef34b72dd2503d147f
Ismael <ismael.lima2000@outlook.com.br>
2024-01-31 13:59:53 -0300

    Calculadora de imc finalizada. Falta finalizar as outras paginas

---
+++ assets/scripts/main.js
@@ -0,0 +1,92 @@
+//imc-page
+const campoAltura = document.querySelector("#altura");
+const campoPeso = document.querySelector("#peso");
+const botao = document.querySelector("#btCalcular");
+const bloco = document.querySelector("#imc-page");
+
+function main() {
+
+    botao.addEventListener(('click'), botaoClicado)
+
+}
+
+function botaoClicado() {
+    const resultado = calcularImc(campoAltura.value, campoPeso.value);
+    exibirTela(resultado);
+}
+
+function calcularImc(altura, peso) {
+    const numeroImc = Math.round((peso/(altura*altura)*10000))
+    console.log(numeroImc)
+    return consultarImc(numeroImc);
+}
+
+function exibirTela(resultado) {
+    if (bloco.querySelector(".caixa-resultado")){
+        const caixa = document.querySelector(".caixa-resultado")
+        caixa.innerHTML = `<h2> Seu resultado foi: </h2></br>
+        <p> ${resultado}  </p> `;
+    }else{
+        const caixa = document.createElement("div");
+        caixa.classList.add("caixa-resultado")
+        caixa.innerHTML = `<h2> Seu resultado foi: </h2></br>
+                       <p> ${resultado}  </p> `;
+        bloco.appendChild(caixa);
+    };
+    
+
+}
+
+function consultarImc(numeroImc) {
+    const lista = [
+        {
+            numero: 24.9,
+            imc: 'Peso Normal'
+        },
+        {
+            numero: 18.5,
+            imc: "Abaixo do Peso"
+        },
+
+        {
+            numero: 29.9,
+            imc: 'Sobrepeso'
+        },
+        {
+            numero: 34.9,
+            imc: 'Obesidade grau 1'
+        },
+        {
+            numero: 39.9,
+            imc: 'Obesidade grau 2'
+        },
+        {
+            numero: 40,
+            imc: 'Obesidade grau 3'
+        }
+    ];
+    lista.sort(function(a, b){return a.numero - b.numero});
+    console.log(lista)
+    if(numeroImc <= lista[0].numero){
+        return  "Abaixo do Peso"
+    }else if( numeroImc > lista[lista.length - 1].numero){
+        return "Gordasso hein pai kkkk"
+    }
+    for (let index = 1; index < lista.length; index++) {
+        if(numeroImc <= lista[index].numero && numeroImc > lista[index - 1].numero && numeroImc < lista[index + 1].numero){
+            return lista[index].imc
+        }
+    }
+}
+
+
+main()
+
+        // if(lista[index].numero >= numeroImc){
+        //     lista[index - 1].numero === undefined ? console.log("undefined") : console.log("Indefinido mas ao menos deu certo a verificacao");
+        // }else if(lista[index].numero >= numeroImc && lista[index - 1].numero < numeroImc ){
+        //     return lista[index].imc
+        // }else{
+        //     lista[index + 1].numero < numeroImc ? console.log("Cheguei a outra verificacao") : console.log("Cheguei a ultima verificacao")
+            
+        // }
